6b9dc93d0a3b47ff8c4c23779fc5ecae94be8e3a
ensht <ensht@DESKTOP-OOAF4HD>
2020-07-13 12:38:40 +0300

    Second commit

--- app/src/main/java/com/maxhryhlevych/app/model/SimilarAnnouncements.java
+++ app/src/main/java/com/maxhryhlevych/app/model/SimilarAnnouncements.java
@@ -1,10 +1,18 @@
 package com.maxhryhlevych.app.model;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.jayway.restassured.path.json.JsonPath;
 import com.maxhryhlevych.app.repo.AnnouncementRepo;
 
 public class SimilarAnnouncements {
@@ -14,20 +22,56 @@ public class SimilarAnnouncements {
 	@Autowired
 	private Announcement announcement;
 	
-	private Announcement post;
+	private Optional<Announcement> post;
 	
-	public SimilarAnnouncements(List<Announcement> threeSimilar) {
-		this.threeSimilar = threeSimilar;
+	public SimilarAnnouncements() {
+		formattingTheSimilarList();
+		findThreeSimilar();
 	}
 
-	public List<Announcement> findSimilar() {
-		return repo.findByTitleAndDescription(announcement.getTitle(), announcement.getDescription());
-	}
+	private List<Optional<Announcement>> findSimilar = new ArrayList<Optional<Announcement>>();
 	
-	private List<Announcement> threeSimilar = new ArrayList<Announcement>();
+	private List<Optional<Announcement>> threeSimilar = new ArrayList<Optional<Announcement>>();
 	
-	public void findThreeSimilar() {
-		int sizeOfSimilarList = findSimilar().size();
+
+    public void formattingTheSimilarList() {
+        Pattern pattern = Pattern.compile("\\w+", Pattern.UNICODE_CHARACTER_CLASS | Pattern.CASE_INSENSITIVE);
+        
+        Matcher matcherOfTitle = pattern.matcher(announcement.getTitle());
+        Matcher matcherOfDescription = pattern.matcher(announcement.getDescription());
+        
+        SortedSet<String> wordsOfTitle = new TreeSet<>();
+        SortedSet<String> wordsOfDescription = new TreeSet<>();
+
+        while (matcherOfTitle.find())
+        	wordsOfTitle.add(matcherOfTitle.group().toLowerCase());
+        while (matcherOfDescription.find())
+        	wordsOfDescription.add(matcherOfDescription.group().toLowerCase());
+
+        for (String wordOfTitle : wordsOfTitle) {
+        	
+        	String findedTitles = repo.findByTitle(wordOfTitle).toString();
+        	
+        	for (String wordOfDescription : wordsOfDescription) {
+        		
+        		String findedDescriptions = repo.findByDescription(wordOfDescription).toString();
+            	
+            	List<Long> idsOfTitles = new ArrayList<Long>(JsonPath.from(findedTitles).get("id"));
+            	List<Long> idsOfDescriptions = new ArrayList<Long>(JsonPath.from(findedDescriptions).get("id"));
+            	idsOfDescriptions.retainAll(idsOfTitles);
+            	
+            	Set<Long> acceptableIds = new HashSet<Long>();
+            	acceptableIds.addAll(idsOfDescriptions);
+            	
+            	for (Long acceptableId : acceptableIds)
+            		findSimilar.add(repo.findById(acceptableId));
+            }
+        }
+    }
+    
+    
+    public void findThreeSimilar() {
+		int sizeOfSimilarList = findSimilar.size();
 		int cycleCount;
 		
 		if (sizeOfSimilarList < 3) {
@@ -37,10 +81,10 @@ public class SimilarAnnouncements {
 		}
 		
 		for (int i = 0; i < cycleCount; i++) {
-			post = findSimilar().get(sizeOfSimilarList - 1);
+			post = findSimilar.get(sizeOfSimilarList - 1);
 			threeSimilar.add(post);
 			sizeOfSimilarList--;
 		}
 	}
-
+    
 }
--- app/src/main/java/com/maxhryhlevych/app/repo/AnnouncementRepo.java
+++ app/src/main/java/com/maxhryhlevych/app/repo/AnnouncementRepo.java
@@ -3,11 +3,16 @@ package com.maxhryhlevych.app.repo;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.maxhryhlevych.app.model.Announcement;
 
 @Repository
 public interface AnnouncementRepo extends JpaRepository<Announcement, Long> {
-	List<Announcement> findByTitleAndDescription(String title, String description);
+	@Query("select a from Announcement a where title like %?1%")
+	List<Announcement> findByTitle(String title);
+	
+	@Query("select a from Announcement a where description like %?1%")
+	List<Announcement> findByDescription(String description);
 }
